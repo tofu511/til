ed65b04ddc58f94fdd962f01aa70b1473b9141c7
Tomoki Furuhashi <mokmok511@gmail.com>
2019-07-11 22:21:34 +0900

    create PrintableInstances.scala

---
+++ cats/PrintableInstances.scala
@@ -0,0 +1,11 @@
+object PrintableInstances {
+	implicit val stringPrinter: Printable[String] =
+		new Printable[String] {
+			def format(value: String): String = value
+		}
+	
+	implicit val intPrinter: Printable[Int] =
+		new Printable[Int] {
+			def format(value: Int): String = value.toString
+		}
+}
