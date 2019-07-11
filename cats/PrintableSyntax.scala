35fa9768ec51ebc1c9135635e278dc4a1710b373
Tomoki Furuhashi <mokmok511@gmail.com>
2019-07-11 22:40:27 +0900

    create PrintableSyntax.scala

---
+++ cats/PrintableSyntax.scala
@@ -0,0 +1,6 @@
+object PrintableSyntax {
+	implicit class PrintableOps[A](value: A) {
+		def format(implicit p: Printable[A]): String = p.format(value)
+		def print(implicit p: Printable[A]): Unit = println(p.format(value))
+	}
+}
